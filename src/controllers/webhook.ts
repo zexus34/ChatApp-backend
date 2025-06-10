94a2db64555ac8cd36d1b7aa84eb374d235719bd
Ankit Jha <ajo91277@gmail.com>
2025-06-11 00:28:27 +0530

    refactor: remove unused user action handling from webhook controller

--- src/controllers/webhook.ts
+++ src/controllers/webhook.ts
@@ -7,38 +7,6 @@ import { ApiResponse } from "../utils/ApiResponse";
 
 const router = Router();
 
-router.post("/", async (req: Request, res: Response) => {
-  const { action, name, avatarUrl } = req.body;
-  const user = req.user;
-  if (!user) {
-    throw new ApiError(403, "Forbidden: Invalid user");
-  }
-  try {
-    if (action === "delete") {
-      await Chat.updateMany(
-        { "participants.userId": user.id },
-        { $pull: { participants: { userId: user.id } } },
-      );
-    } else if (action === "update") {
-      await Chat.updateMany(
-        { "participants.userId": user.id },
-        {
-          $set: {
-            "participants.$.name": name,
-            "participants.$.avatarUrl": avatarUrl,
-          },
-        },
-      );
-    }
-    res
-      .status(200)
-      .json(new ApiResponse(200, null, "User data updated successfully!"));
-  } catch (error) {
-    console.log(error);
-    throw new ApiError(500, "Internal server error");
-  }
-});
-
 export const deleteUserWebhook = async (req: Request, res: Response) => {
   const user = req.user;
   if (!user) {
